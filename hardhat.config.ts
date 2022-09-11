90fba33422417dff2b5c0f911de2948c83b4aed5
cryshado <mirmiclub@gmail.com>
2022-09-11 19:01:29 +0300

    change optimizer settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,8 +21,8 @@ const config: HardhatUserConfig = {
         version: '0.8.9',
         settings: {
             optimizer: {
-                enabled: true,
-                runs: 200
+                enabled: false,
+                runs: 0
             }
         }
     },
